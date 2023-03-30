f7760cd357e77c3d796b0f4616029f409d375d16
PahaN47 <pahan4781@gmail.com>
2023-03-30 03:57:25 +0300

    aaa3

--- src/company_back/settings.py
+++ src/company_back/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "models",
+    "company_back",
 ]
 
 MIDDLEWARE = [
@@ -81,7 +81,7 @@ DATABASES = {
         "USER": "usercompanydb",
         "PASSWORD": "userCompanyDB",
         "HOST": "localhost",
-        "PORT": '',
+        "PORT": "",
     }
 }
 
@@ -126,3 +126,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "company_back.User"
