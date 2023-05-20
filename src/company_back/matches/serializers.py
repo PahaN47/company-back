930d77d5c9654d32d6c16ed836817e6f769f82b5
PahaN47 <pahan4781@gmail.com>
2023-05-21 01:12:50 +0300

    cors + fixes

--- src/company_back/matches/serializers.py
+++ src/company_back/matches/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 
-from company_back.models import Match
+from company_back.media.serializers import MediaSerializer
+from company_back.models import Match, User
+
+
+class MatchUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "avatar", "firstName", "lastName"]
+        read_only_fields = ["id, avatar", "firstName", "lastName"]
+
+    def to_representation(self, instance):
+        avatar = instance.avatar
+        representation = super().to_representation(instance)
+        representation["avatar"] = MediaSerializer(avatar).data["url"]
+
+        return representation
 
 
 class MatchesSerializer(serializers.ModelSerializer):
@@ -8,6 +23,14 @@ class MatchesSerializer(serializers.ModelSerializer):
         model = Match
         fields = "__all__"
         read_only_fields = ["id", "initiator", "reciever", "date"]
+
+    def to_representation(self, instance):
+        initiator = instance.initiator
+        reciever = instance.reciever
+        representation = super().to_representation(instance)
+        representation["initiator"] = MatchUserSerializer(initiator).data
+        representation["reciever"] = MatchUserSerializer(reciever).data
+        return representation
 
 
 class CreateMatchSerializer(serializers.ModelSerializer):
