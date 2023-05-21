a3fbd902598f66b3a7ce50242045d601d5d21cc8
PahaN47 <pahan4781@gmail.com>
2023-05-22 01:44:28 +0300

    profile update

--- src/company_back/profile/views.py
+++ src/company_back/profile/views.py
@@ -1,10 +1,11 @@
+import sys
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from company_back.const import Role
-from company_back.models import User
+from company_back.models import Media, User
 from .serializers import ProfileSerializer
 
 
@@ -16,9 +17,19 @@ class ProfileViewSet(ModelViewSet):
     queryset = User.objects.exclude(role=Role.ADMIN)
 
     def partial_update(self, request, *args, pk=None, **kwargs):
-        user_id = request.user.id
-        if pk != user_id:
+        user = request.user
+        if pk != user.id:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+        kwargs["pk"] = user.id
 
-        kwargs["pk"] = user_id
+        avatar_url = None
+        try:
+            avatar_url = request.data["avatar"]
+            old_avatar_url = Media.objects.get(id=user.avatar.id).url
+        except Exception:
+            old_avatar_url = None
+        if avatar_url and avatar_url != old_avatar_url:
+            new_avatar = Media.objects.create(url=avatar_url)
+            new_avatar.save()
+            request.data["avatar"] = new_avatar.id
         return super().partial_update(request, *args, **kwargs)
