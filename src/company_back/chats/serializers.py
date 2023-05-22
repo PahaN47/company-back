f031f57e91d6b7892db6caea5c3ed1d27ebc39ff
PahaN47 <pahan4781@gmail.com>
2023-05-22 18:18:36 +0300

    chat update

--- src/company_back/chats/serializers.py
+++ src/company_back/chats/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
+from company_back.media.serializers import MediaSerializer
 
-from company_back.models import Chat
+from company_back.models import Chat, User
+
+
+class ChatUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "firstName", "lastName", "avatar"]
+        read_only_fields = ["id", "firstName", "lastName", "avatar"]
+
+    def to_representation(self, instance):
+        avatar = instance.avatar
+        representation = super().to_representation(instance)
+        representation["avatar"] = MediaSerializer(avatar).data["url"]
+        return super().to_representation(instance)
 
 
 class ChatsSerializer(serializers.ModelSerializer):
+    user1 = ChatUserSerializer(read_only=True, many=False)
+    user2 = ChatUserSerializer(read_only=True, many=False)
+
     class Meta:
         model = Chat
         fields = "__all__"
